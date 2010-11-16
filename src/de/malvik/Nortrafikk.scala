01818fd662fee316be8417eebd87edfb0d9dd93c
Sven Malvik <sven@malvik.de>
2010-11-16 21:46:28 +0100

    Hello world

---
+++ src/de/malvik/Nortrafikk.scala
@@ -0,0 +1,11 @@
+package de.malvik;
+
+import android.app.Activity
+import android.os.Bundle
+
+class Nortrafikk extends Activity {
+    override def onCreate(savedInstanceState:Bundle) : Unit = {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main)
+    }
+}
