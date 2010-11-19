69d5616cae834ee61c44e5fadab1c6216ec2f31b
Sven Malvik <sven@malvik.de>
2010-11-19 11:54:35 +0100

    Web

--- src/de/malvik/Nortrafikk.scala
+++ src/de/malvik/Nortrafikk.scala
@@ -2,10 +2,26 @@ package de.malvik;
 
 import android.app.Activity
 import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
 
 class Nortrafikk extends Activity {
     override def onCreate(savedInstanceState:Bundle) : Unit = {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main)
+        
+        val web = findViewById(R.id.webView).asInstanceOf[WebView]
+        web.loadUrl("http://m.trafikanten.no/")
+        web.getSettings().setJavaScriptEnabled(true)
+        web.getSettings().setJavaScriptCanOpenWindowsAutomatically(false)
+        web.getSettings().setPluginsEnabled(false)
+        web.getSettings().setSupportMultipleWindows(false)
+        web.getSettings().setSupportZoom(false)
+        web.setVerticalScrollBarEnabled(false)
+        web.setHorizontalScrollBarEnabled(false)
+        
+        web.setWebViewClient(new WebViewClient {
+        	override def shouldOverrideUrlLoading(view: WebView, url: String) = false
+        });
     }
 }
